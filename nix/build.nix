18788f27cfe40c8f032bea29c5c5baf96da811ff
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2021-12-17 09:51:12 +0100

    nix: use nix-gitignore

--- nix/build.nix
+++ nix/build.nix
@@ -1,14 +1,11 @@
-{
-  cargo,
-  stdenv,
-  wasm-pack,
-  naersk,
+{ naersk
+, nix-gitignore
 }:
 let
   wasmTarget = "wasm32-unknown-unknown";
 in
 naersk.buildPackage {
-  root = ../.;
+  root = nix-gitignore.gitignoreSource [ "nix/" ] ../.;
 
   copyLibs = true;
   CARGO_BUILD_TARGET = wasmTarget;
