95e01fab0d0d02681c962402a62316f9796913e9
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2022-01-06 15:02:09 +0100

    nix: complete packaging + web serving

--- nix/release.nix
+++ nix/release.nix
@@ -13,6 +13,34 @@ let
     rustc = rust;
   };
 in
-{
-  build = pkgs.callPackage ./build.nix { inherit naersk; };
+rec {
+  # Build the rust wasm project resulting in *.wasm + javascript bindings
+  rust-wasm-preset-converter = pkgs.callPackage ./build.nix { inherit naersk; };
+
+  # Bundle the rust wasm project with a html project
+  preset-converter = pkgs.stdenv.mkDerivation {
+    name = "html";
+
+    src = ../.;
+
+    buildCommand = ''
+      mkdir -p $out
+      ln -s ${rust-wasm-preset-converter} $out/rust-preset-converter
+      ln -s $src/index.html $out/index.html
+    '';
+  };
+
+  # Create a script that starts a webserver serving the preset converter
+  http-server = pkgs.writers.writeBash "http-server.sh" ''
+    set -uo pipefail
+    port="8000"
+    ${pkgs.simple-http-server}/bin/simple-http-server --port "$port" -- ${preset-converter} &
+    src_pid=$!
+
+    echo "Serving ${preset-converter} with PID $src_pid"
+
+    trap 'kill $src_pid; exit' INT
+
+    sleep 1000
+  '';
 }
