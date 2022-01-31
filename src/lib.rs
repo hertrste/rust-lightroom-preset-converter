aaa94b7197093e215a47531725e472ce1f91ccf5
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2022-01-31 15:57:35 +0100

    rust-wasm: initialize panic_hook
    This allows us to print infos in the javascript console when the rust
    code panics. Very helpful for debugging.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ mod preset_converter;
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use std::panic;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -22,5 +23,6 @@ pub fn greet() {
 
 #[wasm_bindgen]
 pub fn convert_preset(s : &str) -> String {
+    panic::set_hook(Box::new(console_error_panic_hook::hook));
     preset_converter::convert_preset(s)
 }
