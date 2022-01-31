be79a2aa65579cdc841b150f8bf294f1d625d2a5
Stefan Hertrampf <stefan.hertrampf@cyberus-technology.de>
2022-01-31 15:29:26 +0100

    fix log macro semicolon warning

--- src/utils.rs
+++ src/utils.rs
@@ -16,6 +16,6 @@ extern crate web_sys;
 #[macro_export]
 macro_rules! log {
     ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
+        web_sys::console::log_1(&format!( $( $t )* ).into())
     }
 }
